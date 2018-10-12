73d8277df4c4e828c2cdc21d177b0704390e241f
高辛格 <xg.gao@tianrang-inc.com>
2018-10-12 15:30:45 +0800

    releases

--- releases/test.py
+++ releases/test.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from pi import rational
+from pi import quaternion
 
 # test rational
 r = rational(1, 2)
@@ -21,3 +22,28 @@ print -r1
 print +r1
 print abs(r1)
 print r1.__nonzero__()
+
+# test quaternion
+q = quaternion(1, 2, 3, 4)
+print q.__class__
+print q.__doc__
+print str(q)
+print repr(q)
+print hash(q)
+print q.conjugate()
+print q.inverse()
+print q.r
+print q.i
+print q.j
+print q.k
+
+q1 = quaternion(1, 1, 1, 1)
+q2 = quaternion(2, 2, 2, 2)
+print q1 + q2
+print q1 - q2
+print q1 * q2
+print q1 / q2
+print -q1
+print +q1
+print abs(q1)
+print q1.__nonzero__()
