fa674875e2e5eaa2f27bd63f6bf9bc472944837c
高辛格 <xg.gao@tianrang-inc.com>
2018-10-12 13:54:04 +0800

    setup

--- setup.py
+++ setup.py
@@ -1,1 +1,16 @@
 # -*- coding: utf-8 -*-
+from distutils.core import setup
+from distutils.core import Extension
+
+rational = Extension(
+    name="pi.rational",
+    sources=["pi/rational/rational.c"],
+    depends=["pi/rational/rational.h"],
+)
+
+setup(
+    name="pi", 
+    version="1.0.0",
+    packages=["pi"],
+    ext_modules=[rational]
+)
